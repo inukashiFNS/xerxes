25efdd3c67df9ef861b308ad9d8b3016a5d824b3
inukashi <jetrex.gd@gmail.com>
2023-05-27 23:57:39 +0530

    db and admin panel second update

--- base/views.py
+++ base/views.py
@@ -4,23 +4,20 @@ from .models import Room
 # Create your views here.
 
 
-rooms = [
-    {'id':1, 'name':'Blue Team'},
-    {'id':2, 'name':'Purple Team'},
-    {'id':3, 'name':'Red Team'},
-]
+# rooms = [
+#     {'id':1, 'name':'Blue Team'},
+#     {'id':2, 'name':'Purple Team'},
+#     {'id':3, 'name':'Red Team'},
+# ]
 
 
 
 def home(request):
-    rooms = Room.objects
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i 
+    room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
