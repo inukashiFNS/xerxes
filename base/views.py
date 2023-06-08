ac00a213b24fd921f9cc199d7256387cdea1b00f
inukashi <jetrex.gd@gmail.com>
2023-06-08 23:06:24 +0530

    topics mobile responsiveness changes

--- base/views.py
+++ base/views.py
@@ -73,7 +73,7 @@ def home(request):
     )
     # icontains here like we search for ha then it should give us like hacks related stuff
 
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:7]
     room_count = rooms.count()
     room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
 
@@ -185,3 +185,9 @@ def updateUser(request):
             form.save()
             return redirect('user-profile', pk=user.id)
     return render(request, 'base/update-user.html', {'form': form})
+
+
+def topicsPage(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    topics = Topic.objects.filter(name__icontains=q)
+    return render(request, 'base/topics.html', {'topics': topics})
